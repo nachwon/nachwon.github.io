faef53d1b4e19fc5145d7a60168dac8ca2045ae7
Che1 <nachwon@gmail.com>
2017-11-09 10:49:54 +0900

    is_pair algorythm edited

--- _posts/Algorithm/-2017-10-11-lvl2-multMatrix.py
+++ _posts/Algorithm/-2017-10-11-lvl2-multMatrix.py
@@ -1,17 +1,33 @@
-def productMatrix(A, B):
-    if len(A[0]) == len(B):
-        for i in range(len(A[0])):
-            for j in range(len(A[0])):
-                for k in range(len(B[0])):
-                    print(i, k, k, j)
-                    # print(A[i][k], B[k][j])
+# def productMatrix(A, B):
+#     if len(A[0]) == len(B):
+#         for i in range(len(A[0])):
+#             for j in range(len(A[0])):
+#                 for k in range(len(B[0])):
+#                     print(i, k, k, j)
+#                     # print(A[i][k], B[k][j])
 
 
 
-a = [[ 1, 2 ],
-     [ 2, 3 ],
-     [ 4, 5 ]]
-b = [[ 3, 4, 5 ],
-     [ 5, 6, 7 ]]
+# a = [[ 1, 2 ],
+#      [ 2, 3 ],
+#      [ 4, 5 ]]
+# b = [[ 3, 4, 5 ],
+#      [ 5, 6, 7 ]]
 
-productMatrix(a, b)
+# productMatrix(a, b)
+
+def is_pair(n):
+    for i in n:
+        if i == ')':
+            return False
+
+        elif i == '(':
+            a = n.count('(')
+            b = n.count(')')
+            if a == b:
+                return True
+            else:
+                return False
+            break
+
+print(is_pair("(hello))(()()"))
